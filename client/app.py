e00cdeef4ee21b859b812aabf7c1cbe9ac323773
Guilherme Libardi <guilhermelibardi@hotmail.com>
2023-10-23 21:20:04 -0300

    Rework on response handler

--- client/app.py
+++ client/app.py
@@ -1,13 +1,26 @@
 import socket
+import json
 
 host = 'veecks-rt.zeabur.app'
-port = 443
+port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
-client.send(f'GET / HTTP/1.1\r\nHost: {host}\r\n\r\n'.encode('utf-8'))
-data = client.recv(1024)
-print(data.decode('utf-8'))
+username = input('username: ')
+password = input('password: ')
+
+client.send(f'NEW_SERVICE \n\nusername={username}& \n\npassword={password}& \n\n'.encode('utf-8'))
+data = client.recv(1024).decode('utf-8')
+
+data = json.loads(data)
+if data['status'] == 0:
+    print('Service connected to server.')
+else:
+    print('Error connecting to server.')
+    print(data['message'])
+
+print('raw response:')
+print(data)
 
 client.close()
--- server/app.py
+++ server/app.py
@@ -19,15 +19,54 @@ def main():
     server.listen(os.getenv('MAX_CONNECTIONS', default=5))
 
     print(f"Servidor Socket escutando em {host}:{port}")
-    print(f"Para encerrar o servidor aperte ctrl+c")
+    print(f"Para encerrar o servidor pressione ctrl+c")
 
     while True:
-        cliente, endereco = server.accept()
-        print(f"Conexão recebida de {endereco[0]}:{endereco[1]}")
+        client, address = server.accept()
+        print(f"Conexão recebida de {address[0]}:{address[1]}")
 
         # Inicia uma nova thread para lidar com a conexão
-        th = Thread(target=handle_connection, args=(cliente,))
+        th = Thread(target=handle_connection, args=(client,))
         th.start()
+
+def handle_connection(client_socket):
+    # check if the request is http or raw
+    data = client_socket.recv(1024).decode('utf-8')
+    if data.split(' ')[0] == 'NEW_SERVICE':
+        handle_service(data, client_socket)
+    else:
+        handle_client(data, client_socket)
+
+def handle_client(data, socket):
+    # handle the cleint request from browser, foward request and return the response from service to the client, then closes
+    if not service:
+        socket.send('HTTP/1.1 503 Service Unavailable\n\n'.encode('utf-8'))
+        socket.close()
+        return
+    service.send(data.encode('utf-8'))
+    response = service.recv(1024)
+    socket.send(response)
+    socket.close()
+
+def handle_service(data, socket):
+    global service
+
+    username = data.split('username=')[1].split('&')[0]
+    password = data.split('password=')[1].split('&')[0]
+    if username == os.getenv('USERNAME') and password == os.getenv('PASSWORD'):
+        service = socket
+        response = json.dumps({
+            'status': 0,
+            'message': 'Service connected to server.'
+        })
+        socket.send(response.encode('utf-8'))
+    else:
+        response = json.dumps({
+            'status': 1,
+            'message': 'Invalid username or password.'
+        })
+        socket.send(response.encode('utf-8'))
+        socket.close()
 
 def handle_close(sig, frame):
     global server
@@ -36,49 +75,6 @@ def handle_close(sig, frame):
     print("\nServer closed.")
     exit(0)
 
-def handle_connection(client_socket):
-    # receive the HTTP request from the client
-    request = client_socket.recv(1024).decode('utf-8')
-    print('HTTP request received from client:', request)
-
-    # parse the HTTP request to get the method and path
-    method, path, _ = request.split(' ', 2)
-
-    # check if the request is valid
-    if path == '/' and method == 'GET':
-        handle_client(client_socket)
-    elif path == '/new-service' and method == 'POST':
-        handle_service(request, client_socket)
-    else:
-        response = 'HTTP/1.1 404 Not Found\n\n'
-
-    # send the HTTP response to the client
-    
-    
-    
-    
-
-def handle_client(socket):
-    # handle the cleint request from browser, foward request and return the response from service to the client, then closes
-    response = 'HTTP/1.1 200 OK\n\ncontent-type: text/html\n\n <h1> Hello World! </h1>'
-    socket.send(response.encode('utf-8'))
-    socket.close()
-
-def handle_service(data, socket):
-    global service
-
-    data = data.decode('utf-8')
-    # get password from http data
-    # packet example: 'POST /new-service HTTP/1.1\r\
-    password = data.split('password=')[1].split('&')[0]
-    if password == os.getenv('PASSWORD'):
-        service = socket
-        socket.send('HTTP/1.1 200 OK Service updated.\n\n'.encode('utf-8'))
-    else:
-        socket.send('HTTP/1.1 401 Unauthorized\n\n'.encode('utf-8'))
-        socket.close()
-
-        
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_close)
