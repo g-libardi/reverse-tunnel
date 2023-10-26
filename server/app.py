26a55c8ecc992e739bd4dffe370fe41ab0550da5
Guilherme Libardi <guilhermelibardi@hotmail.com>
2023-10-25 23:18:02 -0300

    fix: forwarded_for

--- server/app.py
+++ server/app.py
@@ -17,9 +17,10 @@ def proxy(path):
     if not service:
         return jsonify({'error': 'Service not connected to proxy'}), 503
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    forwarded_for = request.headers.get('X-Forwarded-For', '')
 
     req_headers = {name: value for (name, value) in request.headers if name.lower() not in excluded_headers}
-    req_headers['X-Forwarded-For'] = f'''{request.remote_addr},{req_headers.get('X-Forwarded-For', '')}'''
+    req_headers['X-Forwarded-For'] = f'''{forwarded_for}, {request.remote_addr}'''
     resp = sio.call('message', {'method': request.method, 'path': path, 'data': request.get_data(), 'headers': req_headers}, to=service)
     headers = [(name, value) for (name, value) in  resp[2].items() if name.lower() not in excluded_headers]
     
